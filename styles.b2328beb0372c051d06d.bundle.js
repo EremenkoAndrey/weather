74828032be7f54464cbe647d57efb7e6f9e237b7
Andrey Eremenko <fr0ntender@yandex.ru>
2016-12-17 18:22:28 +0300

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{339:function(n,o){},625:function(n,o,c){n.exports=c(339)}},[625]);
+webpackJsonp([1,2],{339:function(n,o){},626:function(n,o,c){n.exports=c(339)}},[626]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
